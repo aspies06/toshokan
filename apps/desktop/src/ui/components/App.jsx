6dd7f6a13263b33bb3f5d50b1a47ca3bf7af715b
Brennan Spies <brennanspies@gmail.com>
2026-06-07 19:09:05 -0700

    Minor edit to main page

--- apps/desktop/src/ui/components/App.jsx
+++ apps/desktop/src/ui/components/App.jsx
@@ -4,7 +4,7 @@ import '../style/App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Toshokan LM - Your Personal Library Manager</h1>
+      <h1>Toshokan LM - Your A.I. Personal Library Manager</h1>
       {/* Future UI components will go here */}
     </div>
   );
