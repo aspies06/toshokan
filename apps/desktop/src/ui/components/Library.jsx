ddaf2266824329f4fda21ce0f9c482cede3dafb3
Brennan Spies <brennanspies@gmail.com>
2026-07-06 12:34:23 -0700

    Wire together navigation between Library and Collection pages

--- apps/desktop/src/ui/components/Library.jsx
+++ apps/desktop/src/ui/components/Library.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 function Library() {
   return (
     <div>
       <h2>Library Component</h2>
-      {/* Future library UI components will go here */}
+      <ul>
+        <li><Link to="/collections/1">Collection 1</Link></li>
+        <li><Link to="/collections/2">Collection 2</Link></li>
+        <li><Link to="/collections/3">Collection 3</Link></li>
+      </ul>
     </div>
   );
 }
